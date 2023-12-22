e37612cd2149694ae9b97c559d69b0334c0193ac
Doomaykaka <domaykaka@gmail.com>
2023-12-22 11:30:43 +0400

    Update

---
+++ app/src/main/java/passwordmanager/gui/ui/GroupWindow.java
@@ -0,0 +1,5 @@
+package passwordmanager.gui.ui;
+
+public class GroupWindow {
+
+}
--- app/src/main/java/passwordmanager/gui/ui/MainWindow.java
+++ app/src/main/java/passwordmanager/gui/ui/MainWindow.java
@@ -5,17 +5,26 @@ import com.formdev.flatlaf.FlatDarkLaf;
 import passwordmanager.gui.manager.Logger;
 
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
-import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextField;
+import javax.swing.ScrollPaneLayout;
 import javax.swing.UIManager;
 
 public class MainWindow {
     private JFrame mainFrame;
     private JPanel mainPanel;
+    private JPanel groupPanel;
+    private JScrollPane groupSPane;
 
     public void create() {
         // FlatDarculaLaf.setup();
@@ -34,6 +43,12 @@ public class MainWindow {
 
         finishCreateWindow();
 
+        // -------------------------
+        addPasswordGroupToList("Group1");
+        addPasswordGroupToList("Group2");
+        addPasswordGroupToList("Group3");
+        addPasswordGroupToList("Group4");
+        addPasswordGroupToList("Group5");
     }
 
     public void startCreateWindow() {
@@ -47,30 +62,113 @@ public class MainWindow {
     public void finishCreateWindow() {
         mainFrame.add(mainPanel);
 
-        mainFrame.setPreferredSize(new Dimension(400, 400));
+        mainFrame.setPreferredSize(new Dimension(400, 300));
 
         mainFrame.pack();
+        mainFrame.setResizable(false);
         mainFrame.setVisible(true);
     }
 
     public void generateComponents() {
-        JLabel label = new JLabel("Test label");
-        label.setBounds(150, 80, 100, 20);
+
+        generateMainTitle();
+
+        generateAddButton();
+        generateLoadButton();
+        
+        //Error
+        //generateFilterPanel();
+
+        generatePasswordGroupsList();
+    }
+
+    public void generateMainTitle() {
+        JLabel label = new JLabel("Password groups");
+        label.setBounds(150, 40, 100, 20);
         mainPanel.add(label);
+    }
 
+    public void generateAddButton() {
         JButton button = new JButton();
-        button.setText("Click me");
-        button.setBounds(150, 120, 100, 20);
-        mainPanel.add(button);
+        button.setText("+");
+        button.setBounds(150, 80, 40, 20);
+        //button.setPreferredSize(new Dimension(100, 20));
 
-        JCheckBox checkBox = new JCheckBox();
-        checkBox.setText("Check me");
-        checkBox.setBounds(150, 40, 100, 20);
-        mainPanel.add(checkBox);
+        mainPanel.add(button);
+    }
+
+    public void generateLoadButton() {
+        JButton button = new JButton();
+        button.setText("▼");
+        button.setBounds(210, 80, 40, 20);
+        //button.setPreferredSize(new Dimension(100, 20));
+
+        mainPanel.add(button);
+    }
+
+    public void generateFilterPanel() {
+        FlowLayout layout = new FlowLayout();
+
+        JPanel panel = new JPanel();
+        panel.setLayout(layout);
+        panel.setBounds(20, 120, 360, 80);
 
         JTextField field = new JTextField();
-        field.setText("Input me");
-        field.setBounds(150, 160, 100, 20);
-        mainPanel.add(field);
+        field.setText("Search");
+        JButton searchButton = new JButton();
+        searchButton.setText("find");
+
+        panel.add(field);
+        panel.add(searchButton);
+
+        mainPanel.add(panel);
+    }
+
+    public void generatePasswordGroupsList() {
+        groupPanel = new JPanel();
+        groupPanel.setLayout(new BoxLayout(groupPanel, BoxLayout.Y_AXIS));
+        groupPanel.setBounds(0, 160, 340, 80);
+
+        groupSPane = new JScrollPane(groupPanel, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+                JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        groupSPane.setLayout(new ScrollPaneLayout());
+        
+        groupSPane.setBounds(20, 160, 340, 80);
+        groupSPane.setAutoscrolls(true);
+
+        mainPanel.add(groupSPane);
+    }
+
+    public void addPasswordGroupToList(String groupName) {
+        GridLayout gridLayout = new GridLayout();
+        gridLayout.setColumns(2);
+        gridLayout.setRows(0);
+        gridLayout.setVgap(5);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(gridLayout);
+        panel.setBounds(20, 200, 360, 80);
+
+        JButton button = new JButton();
+        button.setText("Open");
+        button.setBounds(0, 0, 100, 20);
+
+        JLabel label = new JLabel(groupName);
+        label.setBounds(0, 0, 100, 20);
+
+        //
+        button.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                addPasswordGroupToList("Test");
+            }
+        });
+        //
+
+        panel.add(button);
+        panel.add(label);
+
+        groupPanel.add(panel);
     }
 }
