ce22f0fdf011e71471d8bb7aa4eb140d2efb54ea
Doomaykaka <domaykaka@gmail.com>
2024-02-07 02:40:46 +0400

    License added

--- v01dbuilder.py
+++ v01dbuilder.py
@@ -22,7 +22,6 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
-
 from argparse import ArgumentParser
 from logging import DEBUG, INFO, basicConfig, getLogger
 from os import mkdir
